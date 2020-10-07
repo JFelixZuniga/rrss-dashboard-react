b196f18f9841adc3c5744c1cae78bb485b2e29d9
JFelixZuniga <johnfzuniga@gmail.com>
2020-10-06 23:19:20 -0300

    4to commit, Dark Mode finalizado, práctica ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./globals.css";
 import Header from "./header.js";
 import TopCrdList from "./top-card-list";
@@ -6,14 +6,35 @@ import Overview from "./overview";
 import Switch from "./switch.js";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  const mainClass = darkMode ? "is-dark-mode" : "is-light-mode";
+
+  function changeMedia(mq) {
+    setChecked(mq.matches);
+    setDarkMode(mq.matches);
+  }
+
+  useEffect(() => {
+    const mq = window.matchMedia("(prefers-color-scheme: dark)");
+    mq.addListener(changeMedia);
+    setDarkMode(mq.matches);
+    setChecked(mq.matches);
+  }, []);
+
   return (
-    <>
+    <main className={mainClass}>
       <Header>
-        <Switch />
+        <Switch
+          setDarkMode={setDarkMode}
+          checked={checked}
+          setChecked={setChecked}
+        />
       </Header>
       <TopCrdList />
       <Overview />
-    </>
+    </main>
   );
 }
 
